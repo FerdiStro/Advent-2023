f6d6201454af2258bbaa0104b01cc004f3a01a71
ferdinands <ferdinand.strobel@infrontfinance.com>
2023-12-15 22:55:34 +0100

    Part 2 Solution

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,36 @@
 use std::io;
 
+fn map_string_int(mut line: String) -> String{
+    let dec_list : [&str; 9] = ["one", "two", "three", "four", "five", "six" , "seven" , "eight", "nine"];
+    let mut i: i32 = 1;
+    let mut find_first = false;
+    for n in 5..line.len(){
+        if n > line.len() || find_first == true {
+            break;
+        }
+        for dec in dec_list{
+            let fist_dec =   &line[..n];
+            let x = fist_dec.contains(dec);
+            if x == true{
+                line = line.replace(dec, &*i.to_string());
+                find_first = true;
+                break;
+            }
+            i += 1;
+        }
+        i = 1;
+    }
+    return line;
+}
+
+
 fn main() {
- 
     let mut list  = read_input();
     let mut result : usize = 0;
 
     for line in &mut list {
-
-
-        // *line = map_string_int(line.clone());
-
+        *line = map_string_int(line.clone());
         let parts =  line.chars();
-
         let mut line_num = String::new();
 
         for part  in parts {
@@ -21,7 +40,6 @@ fn main() {
         }
 
         let len = line_num.len();
-
         let mut num = String::new();
 
         if len ==  1{
@@ -35,23 +53,18 @@ fn main() {
         }else if len == 0{
             num.push('0');
         }
-
+        println!("{}", num);
         let line_result: Result<i32, _> = num.parse();
-        println!("NUM: {}", num);
-
-
         match line_result {
             Ok(parsed_num) => {
                 result += parsed_num as usize;
-               
             }
             Err(err) => {
                 println!("Error-Converting String to usize-int: {}", err);
              }
         }
-       
     }
-    println!("Teil-1 : {}", result);
+    println!("Solution: {}", result);
 }
 
 fn  read_input() -> Vec<String> {
